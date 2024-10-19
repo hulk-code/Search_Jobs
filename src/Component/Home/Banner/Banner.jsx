a2425cfbffe9ddf478ba65e004d11ea46c4139fc
Akash <shoaibsarkarakash019@gmail.com>
2024-10-19 06:30:09 +0600

    responsive Banner

---
+++ src/Component/Home/Banner/Banner.jsx
@@ -0,0 +1,35 @@
+import user from "../../../assets/images/user.png";
+
+const Banner = () => {
+  return (
+    <div className="lg:flex items-center bg-slate-100">
+      <div className="lg:p-32">
+        <h1 className="text-5xl font-bold">
+          One Step <br /> Closer To Your <br />{" "}
+          <span className="text-red-600">Dream Job</span>
+        </h1>
+        <p>
+          Explore thousands of job opportunities with all the information you
+          need. Its your future. Come find it. Manage all your job application
+          from start to finish.
+        </p>
+        <a href="#_" className="relative inline-block text-lg group m-3">
+          <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+            <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+            <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+            <span className="relative">Get Started</span>
+          </span>
+          <span
+            className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+            data-rounded="rounded-lg"
+          ></span>
+        </a>
+      </div>
+      <div>
+        <img src={user} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
