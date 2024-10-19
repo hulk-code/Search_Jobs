66bd9003bbd100b6f27f8862f62b0aa496f89110
Akash <shoaibsarkarakash019@gmail.com>
2024-10-19 12:39:46 +0600

    dynamic show all job button

--- src/Component/Home/FeaturedJobs/FeaturedJobs.jsx
+++ src/Component/Home/FeaturedJobs/FeaturedJobs.jsx
@@ -27,7 +27,17 @@ const FeaturedJobs = () => {
       >
         Show All
       </button> */}
-     
+      <a
+  href="#_"
+  className={`relative inline-block px-4 py-2 font-medium group m-5 ${jobs.length === dataLength && "hidden"}`}
+  onClick={() => setDataLength(jobs.length)}
+>
+  <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+  <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+  <span className="relative text-black group-hover:text-white">
+    Button Text
+  </span>
+</a>
 
     </div>
   );
