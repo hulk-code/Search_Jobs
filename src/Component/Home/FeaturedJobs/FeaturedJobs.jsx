d4af89485a9e5bf4997074527e0ff74188074bb5
Akash <shoaibsarkarakash019@gmail.com>
2024-10-19 12:38:58 +0600

    complete jobs card

--- src/Component/Home/FeaturedJobs/FeaturedJobs.jsx
+++ src/Component/Home/FeaturedJobs/FeaturedJobs.jsx
@@ -1,26 +1,36 @@
 import { useEffect, useState } from "react";
 import Job from "../Job/job";
 
-
 const FeaturedJobs = () => {
-    const [jobs , setJobs]=useState([])
-    useEffect(() =>{
-        fetch('/public/jobs.json')
-        .then(res => res.json())
-        .then(data => setJobs(data))
-    },[])
-    return (
-        <div>
-           <div>
-                <h1 className="text-3xl text-center font-bold  lg:m-5">Featured Jobs</h1>
-            </div>
-           <div className="lg:w-1/2 mx-auto lg:grid grid-cols-2 ">
-            {
-                jobs.map(job =><Job key={job.id} job={job}></Job>)
-            }
-           </div>
-        </div>
-    );
+  const [jobs, setJobs] = useState([]);
+  const [dataLength, setDataLength] = useState(4);
+  useEffect(() => {
+    fetch("/public/jobs.json")
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
+  return (
+    <div>
+      <div>
+        <h1 className="text-3xl text-center font-bold  lg:m-5">
+          Featured Jobs
+        </h1>
+      </div>
+      <div className="lg:w-2/3 mx-auto lg:grid grid-cols-2 ">
+        {jobs.slice(0, dataLength).map((job) => (
+          <Job key={job.id} job={job}></Job>
+        ))}
+      </div>
+      {/* <button
+        className={jobs.length === dataLength && "hidden"}
+        onClick={() => setDataLength(jobs.length)}
+      >
+        Show All
+      </button> */}
+     
+
+    </div>
+  );
 };
 
 export default FeaturedJobs;
--- src/Component/Home/Job/Job.jsx
+++ src/Component/Home/Job/Job.jsx
@@ -1,7 +1,11 @@
 import logo from "../../../assets/logo/google.png";
+import location1 from "../../../assets/icons/Location.png";
+
+import money from "../../../assets/icons/money.png";
 
 const Job = ({ job }) => {
   const {
+    id,
     job_title,
     company_name,
     remote_or_onsite,
@@ -12,22 +16,24 @@ const Job = ({ job }) => {
   return (
     <div className="border m-5 p-5">
       <img src={logo} alt="" />
-      <h1>{job_title}</h1>
-      <h1>{company_name}</h1>
-      <div className="flex">
-        <p>{remote_or_onsite}</p>
-        <p>{job_type}</p>
+      <h1 className="text-xl font-serif">{job_title}</h1>
+      <h1 className="font-serif">{company_name}</h1>
+      <div className="flex justify-around m-2">
+        <p className="border py-1 px-3 font-serif shadow-sm">{remote_or_onsite}</p>
+        <p className="border py-1 px-3 font-serif shadow-sm">{job_type}</p>
       </div>
-      <div className="flex">
-        <div>{location}</div>
-        <div>{salary}</div>
+      <div className="flex items-center justify-evenly font-serif m-2">
+        <div className="flex"><img src={location1} alt="" /> {location}</div>
+        <div className="flex"> <img src={money} alt="" /> {salary}</div>
       </div>
+      <div className="text-center">
       <a
         href="#_"
         className="rounded relative inline-flex group items-center justify-center
          px-3.5 py-2 m-1 cursor-pointer border-b-4 border-l-2 active:border-purple-600
           active:shadow-none shadow-lg bg-gradient-to-tr from-purple-600 to-purple-500
            border-purple-700 text-white"
+           onClick={`/job/${id}`}
       >
         <span
           className="absolute w-0 h-0 transition-all duration-300 ease-out
@@ -35,6 +41,7 @@ const Job = ({ job }) => {
         ></span>
         <span className="relative">View Details</span>
       </a>
+      </div>
     </div>
   );
 };
